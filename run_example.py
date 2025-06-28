f585baca6b3bf0159cf3dc2928430fd43f8b8fb2
Alexandros Papagiannakis <papagiannakis.alexandros@gmail.com>
2025-06-28 12:46:03 -0700

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -11,7 +11,7 @@ import plot_cell_arrays as arplt
 import cmasher as cmr
 
 
-bio_path = r"D:\Shares\Data_01\Alex Papagiannakis\BioStudies_Data\eLife_Papagiannakis_etal\CJW7323_microfluidics"
+bio_path = r"...\BioStudies_Data\eLife_Papagiannakis_etal\CJW7323_microfluidics"
 
 mother_df = dload.load_cell_cycle_dataframe(bio_path+'/CJW7323_microfluidics_normal_growth')
 oned_df = dload.load_pixel_projections(bio_path+'/CJW7323_one_dimensional_fluorescence_microfluidics')
@@ -24,8 +24,8 @@ meshcolor='white'
 cmaps = (cmr.ember,cmr.cosmic)
 
 array_dict = avcel.generate_movie_arrays(oned_df, mother_df, cc_range, gr_range, [2,3])
-avcel.save_cell_arrays(array_dict, r"D:\Shares\Data_01\Alex Papagiannakis\Python\Analysis_functions\2D_cell_projections"+'/2D_cell_arrays_dict')
-array_dict = avcel.load_cell_arrays(r"D:\Shares\Data_01\Alex Papagiannakis\Python\Analysis_functions\2D_cell_projections"+'/2D_cell_arrays_dict')
+avcel.save_cell_arrays(array_dict, r"...\2D_cell_projections"+'/2D_cell_arrays_dict')
+array_dict = avcel.load_cell_arrays(r"...\2D_cell_projections"+'/2D_cell_arrays_dict')
 
 plot_dict = arplt.plot_cell_projections(array_dict, v_ranges, cmaps, meshcolor, 
-                                        r"D:\Shares\Data_01\Alex Papagiannakis\Python\Analysis_functions\2D_cell_projections\cell_projections")
+                                        r"...\2D_cell_projections\cell_projections")
